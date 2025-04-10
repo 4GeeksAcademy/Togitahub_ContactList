9935690116c5423ecb086d1d4249647c6aa3a79f
Oscar Andres Bolaños Chacon <totmom07@gmail.com>
2025-04-10 04:26:02 +0000

    Mejora en la ceacion de usurio

--- src/App.jsx
+++ src/App.jsx
@@ -6,29 +6,37 @@ import AddContact from "./components/AddContact";
 import EditContact from "./components/EditContact";
 
 function App() {
-    async function createAgenda(slug) {
-        try {
-            const response = await fetch(
-                `https://playground.4geeks.com/contact/agendas/${slug}`,
-                {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                    body: JSON.stringify(),
-                }
-            );
-            const agenda = await response.json();
-            console.log("Agenda creada correctamente");
-        } catch (error) {
-            console.error(error);
-        }
-    }
-
     useEffect(() => {
-        createAgenda('oscar')
+        const createUser = async () => {
+            try {
+                const response = await fetch("https://playground.4geeks.com/contact/agendas/oscar");
+                if (response.ok) {
+                    console.log("La agenda ya existe")
+                } else if (response.status === 404) {
+                    const createResponse = await fetch(
+                        `https://playground.4geeks.com/contact/agendas/${slug}`,
+                        {
+                            method: "POST",
+                            headers: {
+                                "Content-Type": "application/json",
+                            },
+                            body: JSON.stringify(),
+                        }
+                    );
+                    if (createResponse.ok) {
+                        console.log('Agenda creada correctamente');
+                    } else {
+                        console.error(`Error al crear la agenda: ${createResponse.statusText}`);
+                    }
+                } else {
+                    console.error(`Erro al verificar agenda: ${response.statusText}`)
+                }
+            } catch (error) {
+                console.error(`Error en la solicitud: ${error.message}`)
+            }
+        }
+        createUser();
     }, [])
-
 
     return (
         <ContactProvider>
